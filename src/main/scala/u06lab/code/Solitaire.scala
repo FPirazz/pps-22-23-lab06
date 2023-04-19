30b4d5c7542d5a958f4dad1e98dbd2ce7e4c87ca
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-04-19 22:42:04 +0200

    Added working prototype of Exercise 3

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -13,39 +13,82 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
+
+
+
+
+
   def placeMarks(width: Int, height: Int): Unit =
-    val pos: Int = (width * height) / 2
-    var solution: Seq[(Int, Int)] = Seq((pos - (((pos + 1) / width) * width), (pos + 1) / height))
-    var board: Array[Int] = Array(0)
+    val coordX: Int = width + 6
+    val coordY: Int = height + 6
+    val pos: Int = (coordX * coordY) / 2
+    var sol: Seq[(Int, Int)] = Seq((pos - ((pos / coordX) * coordX) - 3, (pos / coordX) - 3))
+    var board: Array[Int] = Array(-1)
+    var count: Int = 0
     for
-      i <- 1 to ((width * height) - 1)
-    do board = board :+ i
+      _ <- 0 until ((width + 6) * (height + 6)) - 1
+    do board = board :+ -1
 
+    for
+      i <- ((width + 6) * 3) + 3 until (((width + 6) * 3) + (3 + width))
+      j <- 1 to height - 1
+    do
+      board(i) = 0
+      board(i+((width + 6) * j)) = 0
+    board(pos) = 1
+
+//    var lol: Int = 1
 //    board.foreach(elem =>
-//      if elem % 7 == 0 && elem != 0 then println()
 //      print(elem + " ")
+//      if lol % 13 == 0 && lol != 0 then println()
+//      lol = lol + 1
 //    )
-
-    def recursivePos(newPos: Int, numPlaced: Int, newBoard: Array[Int]): Unit =
-      if numPlaced == 35 then println(render(solution = solution, width = width, height = height)); println()
+    def recursivePos(newPos: Int, newBoard: Array[Int], oldSol: Seq[(Int, Int)]): Unit =
+      var newSol = oldSol
+//      println("oldSol " + oldSol)
+//      println("newSol " + newSol)
+//      println()
       val possiblePos: Array[Int] = Array(
-        newPos-(width*3), newPos-((width*2)+2), newPos-((width*2)-2), newPos-3, newPos+3, newPos+((width*2)-2), newPos+((width*2)+2), newPos+(width*3)
+        newPos - (coordX * 3), (newPos - (coordX * 2)) + 2, (newPos - (coordX * 2)) - 2, newPos - 3, newPos + 3, (newPos + (coordX * 2)) - 2, (newPos + (coordX * 2)) + 2, newPos + (coordX * 3)
       )
-      possiblePos.foreach(elem =>
-        print(elem + " ")
-      )
-      println()
 
-      possiblePos.foreach(calcPos =>
-        if calcPos>=0 && calcPos<(width*height)-1 && numPlaced != 35 && newBoard(calcPos) == 0
-        then
-          solution = solution :+ ( calcPos - (((calcPos + 1) / width) * width), (calcPos + 1) / height )
-          print(solution)
-          newBoard(newPos) = numPlaced
-          recursivePos(pos, numPlaced + 1, newBoard)
-      )
-    recursivePos(pos, 1, board)
+      for
+        calcPos <- possiblePos
+      do
+        if newBoard(calcPos) != -1 && newBoard(calcPos) == 0 then
+          //          println("elem " + calcPos)
+          newBoard(calcPos) = newBoard(newPos) + 1
+          var x: Int = calcPos - ((calcPos / coordX) * coordX) - 3
+          var y: Int = (calcPos / coordX) - 3
+          if x < 0 then x = 0
+          if y < 0 then y = 0
+          newSol = newSol :+ (x, y)
+
+          if newBoard(calcPos) < 2 then
+
+          //            println("OldPos " + newPos + " - NewPos " + calcPos)
+          //            print("X " + x + " ");
+          //            println("Y " + y)
+          //            println()
+          //            println(newSol)
+            recursivePos(calcPos, newBoard, newSol)
+          else
+//            count = count + 1
+            println(render(solution = newSol.reverse, width = width, height = height))
+            println()
+          newBoard(calcPos) = 0
+          newSol = newSol.dropRight(1)
+    //          print("lol ")
+    //          println(newSol)
+
+    recursivePos(pos, board, sol)
+//    println("Soluzioni sono " + count)
+    @tailrec
+    def lol(ciao: Int, ciao2: Int): Unit =
+      print(ciao + " " + ciao2)
+      lol(for i <- 1 to 10 yield i, for j <- 1 to 10 yield j)
+
 
 //  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
-  placeMarks(width = 7, height = 7)
-
+//  placeMarks(width = 7, height = 7)
+    lol(0, 0)
